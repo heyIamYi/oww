a31295c058eaca8d0925e67458311d8ee91bdb03
A-Zhong <verysimplesetting@gmail.com>
2022-12-18 15:09:31 +0800

    21321

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -46,9 +46,9 @@
                         <div class="box-top d-flex" style="flex-direction: column">
                             <a href="{{ route('googlelogin') }}">google登入</a>
 
-                            <a href="{{route('facebooklogin')}}" class="fb-login-button" data-width="" data-size="large" data-button-type="login_with"
-                                data-layout="rounded"  data-auto-logout-link="true"
-                                data-use-continue-as="false"></a>
+                            <div class="fb-login-button" data-width="" data-size="large" data-button-type="login_with"
+                                data-layout="rounded" onclick="FB_login();" data-auto-logout-link="true"
+                                data-use-continue-as="false"></div>
                         </div>
 
                     </div>
